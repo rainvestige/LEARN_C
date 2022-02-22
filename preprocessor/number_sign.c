7cfcacb28e08cfcbd4843798532d8a16f44a58d3
2111903001 <xinyuloveswork@163.com>
2022-02-22 20:27:07 +0800

    Feat: validate the use of double number sign

---
+++ preprocessor/number_sign.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define concatenate(A, B) A ## B
+
+int main()
+{
+  int concatenate(num, 1) = 1;
+  int concatenate(num, 2) = 2;
+  int concatenate(num, 3) = 3;
+  printf("%d%d%d\n", num1, num2, num3);
+  return 0;
+}
